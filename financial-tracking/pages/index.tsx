53f7c4bb348bb56f3b6eb5d89e3a8386689f9080
Long Pham <61457360+pblongg@users.noreply.github.com>
2022-11-29 11:29:45 +0700

    Update index.tsx

--- financial-tracking/pages/index.tsx
+++ financial-tracking/pages/index.tsx
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import styles from "../styles/Home.module.css";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -20,22 +21,22 @@ export default function Home() {
         </p>
 
         <div className={styles.grid}>
-          <a href="/dashboard" className={styles.card}>
+          <Link href="/dashboard" className={styles.card}>
             <h2>Dashboard &rarr;</h2>
             <p>Quick look on your income and outcome and customize it!</p>
-          </a>
+          </Link>
 
-          <a href="https://nextjs.org/learn" className={styles.card}>
+          <Link href="https://nextjs.org/learn" className={styles.card}>
             <h2>Learn &rarr;</h2>
             <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
+          </Link>
 
-          <a href="https://github.com/vercel/next.js/tree/canary/examples" className={styles.card}>
+          <Link href="https://github.com/vercel/next.js/tree/canary/examples" className={styles.card}>
             <h2>Examples &rarr;</h2>
             <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
+          </Link>
 
-          <a
+          <Link
             href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
             target="_blank"
             rel="noopener noreferrer"
@@ -43,7 +44,7 @@ export default function Home() {
           >
             <h2>Deploy &rarr;</h2>
             <p>Instantly deploy your Next.js site to a public URL with Vercel.</p>
-          </a>
+          </Link>
         </div>
       </main>
 
